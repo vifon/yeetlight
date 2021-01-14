cc26736294ea7c7959dce8b24926cee205e33f4c
Wojciech Siewierski <wojciech@siewierski.eu>
2021-01-14 19:36:48 +0100

    Extract the HTTP method verification to a middleware

--- api/bulb.go
+++ api/bulb.go
@@ -8,89 +8,79 @@ import (
 )
 
 func CallMethod(property string, params... Param) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			addr := r.URL.Query().Get("bulb")
-			if len(addr) == 0 {
+	return Post(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		addr := r.URL.Query().Get("bulb")
+		if len(addr) == 0 {
+			http.Error(w, "400 Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		methodParams := make([]interface{}, len(params))
+		for i := range params {
+			value, err := params[i].Get(r)
+			if err != nil {
 				http.Error(w, "400 Bad Request", http.StatusBadRequest)
 				return
 			}
-
-			methodParams := make([]interface{}, len(params))
-			for i := range params {
-				value, err := params[i].Get(r)
-				if err != nil {
-					http.Error(w, "400 Bad Request", http.StatusBadRequest)
-					return
-				}
-				methodParams[i] = value
-			}
-
-			b := bulb.Bulb{Addr: addr}
-			cmd := bulb.NewCommand(1, property, methodParams...)
-			resp, err := b.Send(cmd)
-			if err != nil {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-			parsed, err := resp.Decode()
-			if err != nil {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-
-			if parsed.Success() == false {
-				w.WriteHeader(http.StatusBadRequest)
-			}
-			w.Write(resp)
-		default:
-			http.Error(w, "405 Method Not Allowed", http.StatusNotFound)
+			methodParams[i] = value
 		}
-	})
+
+		b := bulb.Bulb{Addr: addr}
+		cmd := bulb.NewCommand(1, property, methodParams...)
+		resp, err := b.Send(cmd)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		parsed, err := resp.Decode()
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if parsed.Success() == false {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.Write(resp)
+	}))
 }
 
 func GetInfo(requestedProps... interface{}) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			addr := r.URL.Query().Get("bulb")
-			if len(addr) == 0 {
-				http.Error(w, "400 Bad Request", http.StatusBadRequest)
-				return
-			}
-
-			b := bulb.Bulb{Addr: addr}
-			cmd := bulb.NewCommand(1, "get_prop", requestedProps...)
-			resp, err := b.Send(cmd)
-			if err != nil {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-			parsed, err := resp.Decode()
-			if err != nil {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-
-			if len(parsed.Result) != len(requestedProps) {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-
-			result := make(map[string]string)
-			for i := range requestedProps {
-				result[requestedProps[i].(string)] = parsed.Result[i]
-			}
-			resultJson, err := json.Marshal(result)
-			if err != nil {
-				http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-
-			w.Write(resultJson)
-		default:
-			http.Error(w, "405 Method Not Allowed", http.StatusNotFound)
+	return Get(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		addr := r.URL.Query().Get("bulb")
+		if len(addr) == 0 {
+			http.Error(w, "400 Bad Request", http.StatusBadRequest)
+			return
 		}
-	})
+
+		b := bulb.Bulb{Addr: addr}
+		cmd := bulb.NewCommand(1, "get_prop", requestedProps...)
+		resp, err := b.Send(cmd)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		parsed, err := resp.Decode()
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if len(parsed.Result) != len(requestedProps) {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		result := make(map[string]string)
+		for i := range requestedProps {
+			result[requestedProps[i].(string)] = parsed.Result[i]
+		}
+		resultJson, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(resultJson)
+	}))
 }
--- api/middleware.go
+++ api/middleware.go
@@ -11,3 +11,23 @@ func WithLogging(h http.Handler) http.Handler {
 		h.ServeHTTP(w, r)
 	})
 }
+
+func HttpMethod(method string, h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case method:
+			h.ServeHTTP(w, r)
+		default:
+			log.Printf("Bad method: %v", r.Method)
+			http.Error(w, "405 Method Not Allowed", http.StatusNotFound)
+		}
+	})
+}
+
+func Get(h http.Handler) http.Handler {
+	return HttpMethod(http.MethodGet, h)
+}
+
+func Post(h http.Handler) http.Handler {
+	return HttpMethod(http.MethodPost, h)
+}
