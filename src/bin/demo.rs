d67066e1a5bc6a45a8e584bf1d1b9e4ea78773cc
Wojciech Siewierski <wojciech@siewierski.eu>
2024-04-04 20:39:18 +0200

    Add adjust_bright to the demo

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time::Duration};
+
 use yeetlight::*;
 
 fn main() -> anyhow::Result<()> {
@@ -5,6 +7,11 @@ fn main() -> anyhow::Result<()> {
 
     let b = Bulb::new("192.168.2.162");
     b.set_power(true, Effect::Smooth(500))?;
+    sleep(Duration::from_secs(1));
+    b.set_brightness(Brightness::new(30)?, Effect::Smooth(500))?;
+    sleep(Duration::from_secs(1));
+    b.adjust_brightness(Percentage::new(30)?, Effect::Smooth(500))?;
+    sleep(Duration::from_secs(1));
     b.set_brightness(Brightness::new(100)?, Effect::Smooth(500))?;
     b.set_temperature(Temperature::new(4700)?, Effect::Smooth(500))?;
     // b.set_color(Color::from_hex("FF0000")?, Effect::Smooth(500))?;
